c9b76270a45d8e7d1017f6efd47fb312a6661d9d
Tristar-Alt <Tristanshockman40@gmail.com>
2026-04-29 10:32:14 -0600

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String[][] book;
+
+    public Book(String[][] book) {
+        this.book = book;
+    }
+
+    public Book() {
+        book = new String[10][2];
+    }
+}
